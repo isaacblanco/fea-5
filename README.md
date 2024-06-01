aa20221fb78f8e8b49bc88971f60b50a2facf197
Isaac Blanco <63171746+isaacblanco@users.noreply.github.com>
2024-06-01 18:22:25 +0200

    Agregando fuentes, quitando angular material, poniendo iconos

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
-# PEC5
+# PEC5 - Front End Avanzado (FEA)
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.11.
 
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+
+## Notas sobre el desarrollo
+
+Al principio me cree el directorio en GitHub, pero al crear el proyecto use otro directorio, por lo que los cambios aparecen a partir de cierto punto.
+
+Al trabajar solo, tampoco me centro en ocuparme de ello, por muy buenas costumbres que sean.
+
+Url del proyecto en gitHub: https://github.com/isaacblanco/fea-5
+
+##
+
+## Lectura de JSON
+
+He utilizado una fuente de GNews, tiene la pega que solo permite 100 lecturas diarias, por lo que puede ser un problema si se navega cada página.
+
+En el servicio, hay comentado el formato local que evita el consumo de estas peticiones, tan solo como ejemplo de la forma en que he estado trabajando durante el desarrollo.
+
+## Fuentes
+
+Para evitar problemas, he incluido la fuente Roboto en local, así no hay dependencia de la red para usarla.
+
+## Iconos
+
+Me parece exagerado tener la fuente de Material-design de iconos para dos iconos. Lo he cambiado por dos svg
+
+Adicionalmente, me he tomado la molestia de cambiar los icons de la applicación.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,13 +22,13 @@ import { FormatDatePipe } from './pipes/date-format.pipe';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon'; // Iconos
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Shared modules
-import { SharedModule } from './components/shared/shared.module'; // Importar SharedModule
+import { ServiceWorkerModule } from '@angular/service-worker'; // Importar SharedModule
+import { SharedModule } from './components/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -48,9 +48,14 @@ import { SharedModule } from './components/shared/shared.module'; // Importar Sh
     MatProgressSpinnerModule,
     MatTableModule,
     MatCardModule,
-    MatIconModule,
     MatExpansionModule,
     SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
